0fa248ff06ba443d82d76d76c7614b62fab3f3d1
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-01 14:37:52 +0800

    initial

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const dotenv=require('dotenv');
 const path=require('path');
+const cors=require('cors');
 const connectDb=require('./config/connectDb');
 
 dotenv.config({path:path.join(__dirname,'config','config.env')})
@@ -11,6 +12,8 @@ connectDb();
 const products=require('./routes/product');
 const orders=require('./routes/order');
 
+app.use(express.json());
+app.use(cors());
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
 
