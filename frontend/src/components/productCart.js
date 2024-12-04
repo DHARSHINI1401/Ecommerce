017232577619d14495910c25f3b3bdeb255d86c8
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-04 13:22:35 +0800

    changes

--- frontend/src/components/productCart.js
+++ frontend/src/components/productCart.js
@@ -1,26 +1,27 @@
-export default function ProductCart() {
-    return <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-        <div className="card p-3 rounded">
-            <img
-                className="card-img-top mx-auto"
-                src="./images/products/1.jpeg"
-            />
-            <div className="card-body d-flex flex-column">
-                <h5 className="card-title">
-                    <a href="">OPPO F21s Pro 5G (Dawnlight Gold, 8GB RAM, 128 Storage) with No Cost EMI/Additional Exchange Offers</a>
-                </h5>
-                <div className="ratings mt-auto">
-                    <div className="rating-outer">
-                        <div className="rating-inner"></div>
+export default function ProductCart({ product }) {
+    return (
+        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+            <div className="card p-3 rounded">
+                <img
+                    className="card-img-top mx-auto"
+                    src={product.images[0].image } 
+                    alt={product.name}
+                />
+                <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">
+                        <a href="">{product.name}</a>
+                    </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
                     </div>
+                    <p className="card-text">{product.price}</p>
+                    <a href="#" id="view_btn" className="btn btn-block">
+                        View Details
+                    </a>
                 </div>
-                <p className="card-text">$245.67</p>
-                <a href="#" id="view_btn" className="btn btn-block">View Details</a>
             </div>
         </div>
-    </div>
-
-
-
-
+    );
 }
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,25 +2,24 @@ import { Fragment, useEffect, useState } from 'react';
 import ProductCart from '../components/productCart';
 
 export default function Home() {
-
     const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
-       fetch(process.env.REACT_APP_API_URL +'/product')
-       .then(res => res.json())
-       .then(res =>setProducts(res))
-    },[])
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + '/product')
+            .then((res) => res.json())
+            .then((res) => setProducts(res.products || [])); 
+    }, []);
 
-    return <Fragment>
-        <h1 id="products_heading">Latest Products</h1>
-
-        <section id="products" className="container mt-5">
-            < div className="row" >
-                <ProductCart />
-            </div>
-        </section>
-
-
-
-    </Fragment>
+    return (
+        <Fragment>
+            <h1 id="products_heading">Latest Products</h1>
+            <section id="products" className="container mt-5">
+                <div className="row">
+                    {products.map((product) => (
+                        <ProductCart key={product._id} product={product} />
+                    ))}
+                </div>
+            </section>
+        </Fragment>
+    );
 }
