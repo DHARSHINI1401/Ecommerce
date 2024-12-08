235098bf4b42471eec2d575231e95cc3368ff62e
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-08 14:06:31 +0800

    changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,29 +1,50 @@
 const ProductModel = require('../models/ProductModel');
 
-//Get product api - /api/v1/product
-exports.getProduct = async(req, res, next)=>{
+// Get product API - /api/v1/product
+exports.getProduct = async (req, res, next) => {
+    try {
+        // Create a query object based on the keyword
+        const query = req.query.keyword 
+            ? { name: { $regex: req.query.keyword, $options: "i" } } 
+            : {};
 
-    const products = await ProductModel.find({});
+        const products = await ProductModel.find(query);
 
-    res.json({
-        sucess: true,
-        products
-    })
-}
-//Get single product api - /api/v1/product/:id 
-exports.getSingleProduct=async(req, res, next)=>{
-    try{
-        const product= await ProductModel.findById(req.params.id);
         res.json({
-            sucess:true,
-            product
-        })
-    }catch(error){
-        res.json({
-            sucess:false,
-            message:"product not found!"
-        })
+            success: true,
+            products,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Server error",
+        });
     }
-    
-    
-}
+};
+
+// Get single product API - /api/v1/product/:id 
+exports.getSingleProduct = async (req, res, next) => {
+    try {
+        // Fetch product by ID
+        const product = await ProductModel.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found!",
+            });
+        }
+
+        
+        res.json({
+            success: true,
+            product,
+        });
+    } catch (error) {
+        
+        res.status(500).json({
+            success: false,
+            message: "Server error",
+        });
+    }
+};
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,22 +1,24 @@
 import { Fragment, useEffect, useState } from 'react';
 import ProductCart from '../components/productCart';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
 
     const [products, setProducts] = useState([]);
+    const [searchParams,setsearchParams]=useSearchParams();
 
     useEffect(()=>{
-       fetch(process.env.REACT_APP_API_URL +'/product')
+       fetch(process.env.REACT_APP_API_URL +'/product?'+ searchParams)
        .then(res => res.json())
        .then(res =>setProducts(res.products))
-    },[])
+    },[searchParams])
 
     return <Fragment>
         <h1 id="products_heading">Latest Products</h1>
 
         <section id="products" className="container mt-5">
             < div className="row" >
-                {products.map(product=> <ProductCart  product={product}/>)}
+                { products.map(product=> <ProductCart  product={product}/>)}
                 
             </div>
         </section>
