26f8d6ed5fbbddc3bb246355b93135b687e97265
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-08 14:24:58 +0800

    productpagecreation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Home from './pages/home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductDetail from './pages/productDetail';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
           <Routes>
             < Route path="/" element ={ <Home /> } />
             < Route path="/search" element ={ <Home /> } />
+            < Route path="/product/:id" element ={ <ProductDetail /> } />
+
           </Routes>
         </div>
       </Router>
