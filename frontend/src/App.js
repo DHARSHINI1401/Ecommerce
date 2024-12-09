58cb8b340bf7331ef876089e4dfdfe29934e863a
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-09 14:02:04 +0800

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,25 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
 import Home from './pages/home';
+import {useState} from 'react';
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductDetail from './pages/productDetail';
-
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [cartItems,setCartItems] = useState([]);
+
   return (
     <div className="App">
       <Router>
         <div>
-          <Header />
+          <ToastContainer theme='light' position='top-center'/>
+          <Header cartItems={cartItems} />
           <Routes>
             < Route path="/" element ={ <Home /> } />
             < Route path="/search" element ={ <Home /> } />
-            < Route path="/product/:id" element ={ <ProductDetail /> } />
+            < Route path="/product/:id" element ={ <ProductDetail cartItems={cartItems} setCartItems={setCartItems}/> } />
             < Route path="*" element={<h1>404: Page Not Found</h1>} />
           </Routes>
         </div>
