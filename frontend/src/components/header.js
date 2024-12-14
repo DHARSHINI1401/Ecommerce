575ca9b1a839f7020c6d9f817717946e447cafcb
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-14 12:32:30 +0800

    changes

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,25 +1,39 @@
 import { Link } from "react-router-dom"
 import Search from "./search"
 
-export default function Header({cartItems}) {
+
+export default function Header({ cartItems }) {
     return <nav className="navbar row">
         <div className="col-12 col-md-3">
-            <div className="navbar-brand"> 
-                <h2>SHOPPY<span>KART</span></h2>
-                {/* <Link to='/'> <img width="150px" src="/images/logo.png" /></Link> */}
+            <div className="navbar-brand">
+
+                <Link to='/'> <h2>SHOPPY<span style={{ color: "red" }}>KART</span></h2></Link>
             </div>
+        </div>
+
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+            <Link to={'/'}>
+                <span id="cart" className="ml-5">HOME</span>
+            </Link>
+            <Link to={'/'}>
+                <span id="cart" className="ml-5">ABOUT</span>
+            </Link>
+            <Link to={'/'}>
+                <span id="cart" className="ml-5">PRODUCTS</span>
+            </Link>
         </div>
 
         <div className="col-12 col-md-2 mt-2 mt-md-0">
             <Search />
         </div>
 
+
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
             <Link to={'/cart'}>
-            <span id="cart" className="ml-3">Cart</span>
-            <span className="ml-1" id="cart_count">{cartItems.length}</span> 
+                <span id="cart" className="ml-3">Cart</span>
+                <span className="ml-1" id="cart_count">{cartItems.length}</span>
             </Link>
-            
+
         </div>
     </nav>
 }
