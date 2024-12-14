fa3f12544cea3a46de09398c80300ed6ab7f9059
DHARSHINI1401 <dharshinilakshmanan1401@gmail.com>
2024-12-14 16:49:19 +0800

    changes

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,7 +1,67 @@
 export default function Footer() {
-    return <footer className="py-1 bg-dark">
-        <p className="text-center text-white mt-1 ">
-            JVLcart - 2023-2024, All Rights Reserved
-        </p>
-    </footer>
+    return (<footer id="footer" style={{ marginTop: "50px" }}>
+        <div className="footer-top">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 footer-content">
+                        <h3>ShoppyKart</h3>
+                        <p>Shop the latest trends, exclusive deals, and top-quality products, all in one place!</p>
+
+                        <p>
+                            India <br/>
+                                London <br/>
+                                    France <br/>
+                                    </p>
+                                    
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 footer-links">
+                                        <h4>Usefull Links</h4>
+                                        <ul>
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="about.html">About</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="#">Services</a></li>
+                                            <li><a href="#">Blog</a></li>
+                                            <li><a href="#">Privacy policay</a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 footer-links">
+                                        <h4>Information</h4>
+                                        <ul>
+                                            <li><a href="#">Terms & Conditions</a></li>
+                                            <li><a href="#">Delivery Terms</a></li>
+                                            <li><a href="#">Returns Policy</a></li>
+                                            <li><a href="#">Privacy Policy</a></li>
+                                            <li><a href="#">FAQ</a></li>
+                                            <li><a href="#">The Shop</a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-lg-3 col-md-6 footer-links">
+                                        <h4>Address</h4>
+                                        <p>London, UK
+                                         13h Street, Office 180</p>
+                                        <strong><i className="fas fa-phone"></i> Phone: <strong>+000000000000000</strong></strong><br />
+                                        <strong><i className="fa-solid fa-envelope"></i> Email: <strong>shoppykart@gmail.com</strong></strong>
+                                        <div className="socail-links mt-3">
+                                            <a href="#" className="twiiter"><i class="fa-brands fa-twitter"></i></a>
+                                            <a href="#" className="twiiter"><i class="fa-brands fa-facebook-f"></i></a>
+                                            <a href="#" className="twiiter"><i class="fa-brands fa-google-plus"></i></a>
+                                            <a href="#" className="twiiter"><i class="fa-brands fa-instagram"></i></a>
+                                            <a href="#" className="twiiter"><i class="fa-brands fa-linkedin-in"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    <hr style={{
+                        color:"white"}}/>
+                        <div class="container py-4">
+                            <div class="copyright">
+                                &copy; Copyright <strong>ShoppyKart</strong>.All Rights Reserved |  Designed By <a href="#">DHARSHINI</a>
+                            </div>
+                           
+                            </div>
+                        
+                    </footer>
+)
 }
